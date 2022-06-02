a7159eb04f6f3452c1131ab362ed7e04dc4b5c63
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-06-03 00:05:14 +0300

    menu toggle btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
   <header class="header">
     <!--  -->
-    <div class="menu">menu</div>
-    <font-awesome-icon icon="bars" />
-    <!-- <div class="logo"><img src="src/assets/LogoWhite.png" /></div> -->
+    <div class="toggle-menu-btn">
+      <font-awesome-icon icon="bars" />
+    </div>
+    <div class="logo"><img src="src/assets/LogoWhite.png" /></div>
   </header>
 </template>
 
@@ -23,8 +24,25 @@ export default {
   flex-direction: row;
   background: white;
   border-bottom: 1px solid red;
+  align-items: center;
+  padding: 0px 20px;
 }
+
+.toggle-menu-btn {
+  width: 40px;
+  height: 40px;
+  background: red;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: var(--borderRadius);
+}
+
 .logo {
+  margin-left: auto;
+  display: flex;
   img {
     height: 100%;
     width: 100%;
