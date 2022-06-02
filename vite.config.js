4110adbb1ee80a5662594e1d52b2c1083aafa7e0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-06-03 00:27:01 +0300

    variables

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         additionalData: `
           @import "./src/styles/global.scss";
           @import "./src/styles/_mixins.scss";
+          @import "./src/styles/_variables.scss";
         `,
       },
     },
