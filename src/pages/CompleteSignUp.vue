7c90eaf6354f283192ee13824e546b282219c33b
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-06-17 01:15:59 +0300

    redirecting to complete-signup-page

--- src/pages/CompleteSignUp.vue
+++ src/pages/CompleteSignUp.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>Complete Sign Up</h1>
+  <div class="signup-wrapper">
+    <login-form-vue />
+  </div>
 </template>
 
 <script>
+import LoginFormVue from "../components/LoginForm.vue";
 export default {
   name: "CompleteSignUp",
+  components: { LoginFormVue },
 };
 </script>
+
+<style lang="scss">
+.signup-wrapper {
+  background: red;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
