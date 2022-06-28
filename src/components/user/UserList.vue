aeef6496bd3134365809180ea58d77561269bc9e
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-06-29 00:46:09 +0300

    init user fetch

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="user-list">User list</div>
+  <pre>{{ users }}</pre>
 </template>
 <script>
 import api from "../../mixins/api";
 export default {
   name: "UserList",
+  data() {
+    return {
+      users: [],
+    };
+  },
   mixins: [api],
+  methods: {
+    async fetchUsers() {
+      let data = await this.listUsers();
+      if (data.ok) {
+        this.users = data.users;
+      } else {
+        this.$message({
+          message: data.error || "Error",
+          duration: 5000,
+          type: "error",
+        });
+      }
+    },
+  },
   mounted() {
-    console.log("LIST USERS RUNS");
-    this.listUsers();
+    this.fetchUsers();
   },
 };
 </script>
