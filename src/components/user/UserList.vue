014abb803af402e4fb1ec2c3e1ae7d37bece7935
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-07-01 00:41:25 +0300

    users init

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -5,11 +5,13 @@
   <div class="user-list">
     <div class="userCard" v-for="user in users" :key="user.User_id">
       <!-- <font-awesome-icon :icon="iconsMap[user.Profession.Title]" /> -->
-      <div class="basic-info">
+      <div class="upper">
         <div class="fullname">{{ user.Name }} {{ user.Surname }}</div>
-        <div class="job">{{ user.Profession.Title }}</div>
+        <div class="role">
+          <div class="role-tag">{{ user.Profession.Title }}</div>
+        </div>
       </div>
-      <div class="contact-info">
+      <div class="down">
         <div class="email">{{ user.Email }}</div>
       </div>
     </div>
@@ -61,8 +63,67 @@ export default {
   margin-bottom: 10px;
 
   display: flex;
-  div {
-    margin-left: 10px;
+  flex-direction: column;
+
+  .upper {
+    display: flex;
+    flex-direction: row;
+    .fullname {
+      font-style: normal;
+      font-weight: 700;
+      font-size: 1em;
+      line-height: 22px;
+      color: #000000;
+      width: 300px;
+    }
+
+    .role {
+      width: 300px;
+      .role-tag {
+        background: #888c9b;
+        border-radius: 4px;
+        text-align: center;
+        width: auto;
+        font-size: 12px;
+        color: white;
+        padding: 0px 4px;
+        line-height: 22px;
+        width: fit-content;
+        margin-left: auto;
+      }
+    }
+  }
+
+  .down {
+    margin-top: 15px;
+  }
+
+  .email {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.85rem;
+    line-height: 14px;
+    /* identical to box height, or 156% */
+
+    display: flex;
+    align-items: center;
+    text-decoration-line: underline;
+    font-feature-settings: "tnum" on, "lnum" on;
+
+    color: #888c9b;
+  }
+}
+
+@media (min-width: 768px) {
+  .userCard {
+    flex-direction: row;
+    .upper {
+      .role {
+        .role-tag {
+          margin-left: unset;
+        }
+      }
+    }
   }
 }
 </style>
