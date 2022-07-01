7bf2390f51e166045c9122ac2e61f58e06e18f4f
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-07-02 02:49:21 +0300

    mobile card ok

---
+++ src/components/user/UserCard.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="userCard">
+    <!-- <font-awesome-icon :icon="iconsMap[user.Profession.Title]" /> -->
+    <div class="avatar">
+      <div class="initials">
+        {{ user.Name[0].toUpperCase() }} {{ user.Surname[0].toUpperCase() }}
+      </div>
+    </div>
+    <div class="info">
+      <div class="info-top">
+        <div class="fullname">{{ user.Name }} {{ user.Surname }}</div>
+        <div class="role">
+          <div class="role-tag">{{ user.Profession.Title }}</div>
+        </div>
+      </div>
+      <div class="info-bottom">
+        <div class="email">{{ user.Email }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  props: {
+    user: Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss">
+.userCard {
+  @include card;
+  padding: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-wrap: wrap;
+
+  .avatar {
+    background: blue;
+    border-radius: 50%;
+    width: 45px;
+    height: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    margin-right: 10px;
+    flex-shrink: 0;
+    .initials {
+      letter-spacing: -2px;
+      margin: auto;
+      font-weight: bold;
+    }
+  }
+
+  .info {
+    // border: 1px solid blue;
+    min-width: 285px;
+    .info-top {
+      display: flex;
+      //   border: 1px solid red;
+      margin-bottom: 15px;
+      width: 100%;
+    }
+  }
+
+  .fullname {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1em;
+    line-height: 22px;
+    color: #000000;
+  }
+
+  .role {
+    margin-left: auto;
+    .role-tag {
+      background: #888c9b;
+      border-radius: 4px;
+      text-align: center;
+      width: auto;
+      font-size: 10px;
+      color: white;
+      padding: 0px 4px;
+      line-height: 22px;
+      width: fit-content;
+      margin-left: auto;
+    }
+  }
+
+  .email {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.85rem;
+    line-height: 14px;
+    /* identical to box height, or 156% */
+
+    display: flex;
+    align-items: center;
+    text-decoration-line: underline;
+    font-feature-settings: "tnum" on, "lnum" on;
+
+    color: #888c9b;
+  }
+}
+@media (min-width: 768px) {
+  .userCard {
+  }
+}
+</style>
--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="user-list-view">User list</div>
-  <!-- <pre>{{ users }}</pre> -->
-  <!-- user name, surname, email, reportsCount -->
   <div class="user-list">
-    <div class="userCard" v-for="user in users" :key="user.User_id">
-      <!-- <font-awesome-icon :icon="iconsMap[user.Profession.Title]" /> -->
-      <div class="upper">
-        <div class="fullname">{{ user.Name }} {{ user.Surname }}</div>
-        <div class="role">
-          <div class="role-tag">{{ user.Profession.Title }}</div>
-        </div>
-      </div>
-      <div class="down">
-        <div class="email">{{ user.Email }}</div>
-      </div>
-    </div>
+    <UserCard v-for="user in users" :key="user.User_id" :user="user" />
   </div>
 </template>
 <script>
 import api from "../../mixins/api";
+import UserCard from "./UserCard.vue";
 export default {
   name: "UserList",
+  components: { UserCard },
   data() {
     return {
       users: [],
@@ -52,78 +41,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.user-list {
-  // display: flex;
-}
-.userCard {
-  @include card;
-  margin: unset;
-  max-width: unset;
-  margin-bottom: 10px;
-
-  display: flex;
-  flex-direction: column;
-
-  .upper {
-    display: flex;
-    flex-direction: row;
-    .fullname {
-      font-style: normal;
-      font-weight: 700;
-      font-size: 1em;
-      line-height: 22px;
-      color: #000000;
-      width: 300px;
-    }
-
-    .role {
-      width: 300px;
-      .role-tag {
-        background: #888c9b;
-        border-radius: 4px;
-        text-align: center;
-        width: auto;
-        font-size: 12px;
-        color: white;
-        padding: 0px 4px;
-        line-height: 22px;
-        width: fit-content;
-        margin-left: auto;
-      }
-    }
-  }
-
-  .down {
-    margin-top: 15px;
-  }
-
-  .email {
-    font-style: normal;
-    font-weight: 400;
-    font-size: 0.85rem;
-    line-height: 14px;
-    /* identical to box height, or 156% */
-
-    display: flex;
-    align-items: center;
-    text-decoration-line: underline;
-    font-feature-settings: "tnum" on, "lnum" on;
-
-    color: #888c9b;
-  }
-}
-
-@media (min-width: 768px) {
-  .userCard {
-    flex-direction: row;
-    .upper {
-      .role {
-        .role-tag {
-          margin-left: unset;
-        }
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
