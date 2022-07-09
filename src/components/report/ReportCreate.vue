884c36705624290e2954cf6aca14ec6382d82d05
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-07-09 18:51:04 +0300

    report ui init

--- src/components/report/ReportCreate.vue
+++ src/components/report/ReportCreate.vue
@@ -1,66 +1,168 @@
 <template>
   from to
   <el-form :model="formData" :rules="rules" ref="formData">
+    <el-divider content-position="right"> Professional Information </el-divider>
     <el-form-item label="Name" prop="Name">
       <el-input :disabled="loading" v-model="formData.Name"></el-input>
     </el-form-item>
     <el-form-item label="Surname" prop="Surname">
       <el-input :disabled="loading" v-model="formData.Surname"></el-input>
     </el-form-item>
-    <el-form-item label="AFM" prop="AFM">
+    <el-form-item label="Profession" prop="Profession">
+      <el-input :disabled="loading" v-model="formData.Profession"></el-input>
+    </el-form-item>
+    <el-divider content-position="right"> Patient Information </el-divider>
+    <el-form-item label="Fullname" prop="PatientFullname">
+      <el-input
+        :disabled="loading"
+        v-model="formData.PatientFullname"
+      ></el-input>
+    </el-form-item>
+    <el-form-item label=" AMKA" prop="PatientAMKA">
       <el-input-number
         :controls="false"
         :disabled="loading"
         type="number"
-        v-model="formData.AFM"
+        v-model="formData.PatientAMKA"
       ></el-input-number>
     </el-form-item>
-    <el-form-item label="AMKA" prop="AMKA">
-      <el-input-number
-        :controls="false"
-        :disabled="loading"
-        type="number"
-        v-model="formData.AMKA"
-      ></el-input-number>
-    </el-form-item>
-    <el-form-item label="Profession" prop="Role_id">
-      <el-select
-        :disabled="loading"
-        v-model="formData.Role_id"
-        placeholder="Profession"
-      >
-        <el-option
-          v-for="role in availableRoles"
-          :key="role.Role_id"
-          :value="role.Role_id"
-          :label="role.Title"
-        >
-        </el-option>
-      </el-select>
+    <el-form-item label=" Health Security" prop="PatientHealthSecurity">
+      <el-switch
+        active-text="Yes"
+        inactive-text="No"
+        v-model="formData.PatientHealthSecurity"
+      />
     </el-form-item>
 
+    <el-divider content-position="right"> Address </el-divider>
+    <div class="address-inputs">
+      <el-form-item label="Street" prop="Street">
+        <el-input
+          v-model="formData.PatientAddress.Street"
+          placeholder="Street"
+          type="text"
+        />
+      </el-form-item>
+      <el-form-item label="Number" prop="Number">
+        <el-input-number
+          placeholder="Street number"
+          :controls="false"
+          :disabled="loading"
+          type="number"
+          v-model="formData.PatientAddress.Number"
+        ></el-input-number>
+      </el-form-item>
+
+      <el-form-item label="City" prop="City">
+        <el-input
+          v-model="formData.PatientAddress.City"
+          placeholder="City"
+          type="text"
+        />
+      </el-form-item>
+      <el-form-item label="Postal Code" prop="PostalCode">
+        <el-input-number
+          placeholder="Postal Code"
+          :controls="false"
+          :disabled="loading"
+          type="number"
+          v-model="formData.PatientAddress.PostalCode"
+        ></el-input-number>
+      </el-form-item>
+    </div>
+    <el-divider content-position="right"> Duration of Visit</el-divider>
+
+    <el-form-item>
+      <el-col :span="24">
+        <el-form-item label="Start" prop="date1">
+          <el-date-picker
+            style="width: 100%"
+            v-model="formData.VisitDuration.Start"
+            type="datetime"
+            placeholder="Pick a Date and Time"
+            format="DD/MM/YYYY HH:mm:ss"
+            value-format="x"
+          />
+        </el-form-item>
+      </el-col>
+      <el-col class="text-center" :span="2"> </el-col>
+      <el-col :span="24">
+        <el-form-item label="End" prop="date2">
+          <el-date-picker
+            :style="`width: 100%`"
+            v-model="formData.VisitDuration.End"
+            type="datetime"
+            placeholder="Pick a Date and Time"
+            format="DD/MM/YYYY HH:mm:ss"
+            value-format="x"
+          />
+        </el-form-item>
+      </el-col>
+    </el-form-item>
+    <el-divider content-position="right"> Report </el-divider>
+    <el-form-item label="Delivered Services">
+      <el-select
+        v-model="formData.DeliveredServices"
+        multiple
+        placeholder="Select"
+        style="width: 100%"
+      >
+        <el-option
+          v-for="service in userData.Services"
+          :key="service.Service_id"
+          :label="service.Title"
+          :value="service.Service_id"
+        />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Report">
+      <el-input
+        v-model="formData.ReportCoontent"
+        :autosize="{ minRows: 4 }"
+        type="textarea"
+      ></el-input>
+    </el-form-item>
+
+    <!-- controls -->
     <div class="submit-btn-wrapper">
       <button
         @click="submitForm($event, 'formData')"
         class="form-button create"
       >
-        <span v-if="!loading" class="text"> Create </span>
+        <span v-if="!loading" class="text"> Save </span>
         <Spinner v-if="loading" />
       </button>
     </div>
   </el-form>
+  <pre>{{ formData }}</pre>
 </template>
 <script>
+import Spinner from "@/components/Spinner.vue";
 export default {
   name: "ReportCreate",
+  components: { Spinner },
   data() {
     return {
+      loading: false,
       formData: {
         Name: "",
         Surname: "",
-        AFM: null,
-        AMKA: null,
-        Role_id: null,
+        Profession: "",
+        PatientFullname: "",
+        PatientAMKA: null,
+        PatientHealthSecurity: null,
+        PatientAddress: {
+          Street: "",
+          Number: null,
+          City: "",
+          PostalCode: null,
+        },
+        VisitDuration: {
+          Start: null,
+          End: null,
+        },
+        DeliveredServices: [],
+        ReportCoontent: "",
       },
       rules: {
         Name: [
@@ -120,6 +222,10 @@ export default {
 .report-view {
   .el-form {
     @include custom-element-form;
+  }
+
+  .address-inputs {
+    border: 1px solid red;
   }
 }
 </style>
